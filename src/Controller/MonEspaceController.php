9f029aeaaecc0f5b16d635148d1c5d5f1a3b22b0
Mélody Delage <angie-island@hotmail.fr>
2026-05-11 21:47:31 +0200

    ajout de la page mon espace

--- src/Controller/MonEspaceController.php
+++ src/Controller/MonEspaceController.php
@@ -5,14 +5,26 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\RegionRepository;
+use App\Repository\DepartementRepository;
+use App\Repository\VilleRepository;
 
 final class MonEspaceController extends AbstractController
 {
-    #[Route('/mon/espace', name: 'app_mon_espace')]
+    public function __construct(
+        private RegionRepository $regionRepository,
+        private DepartementRepository $departementRepository,
+        private VilleRepository $villeRepository,
+    ) {}
+    
+   #[Route('/mon-espace', name: 'app_mon_espace')]
     public function index(): Response
     {
         return $this->render('mon_espace/index.html.twig', [
-            'controller_name' => 'MonEspaceController',
+            'regions' => $this->regionRepository->findAll(),
+            'departements' => $this->departementRepository->findAll(),
+            'villes' => $this->villeRepository->findAll(),
         ]);
     }
+
 }
